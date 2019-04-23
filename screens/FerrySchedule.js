88ce4a7ec87f258d22968842ecefe864737ab5d5
Mikaal Naik <mikaalnaik@gmail.com>
2019-04-23 17:26:42 -0400

    Inital hardcoding of ferry schedule

--- screens/FerrySchedule.js
+++ screens/FerrySchedule.js
@@ -7,20 +7,34 @@ import {
 } from 'react-native';
 import BeachImageSelector from '../components/BeachImageSelector';
 import Images from '../assets/beachImages.js';
+import moment from 'moment';
+
+
+const WARDS_FROM_CITY_MON_FRI = ['6:35am', '7am', '7:30am', '8am', '8:45am', '9:45am', '10:45am', '11:45am',
+  '12:45pm', '1:45pm', '2:45pm', '3:15pm', '3:45pm', '4:15pm', '4:45pm', '5:30pm', '6:15pm', '7pm', '7:45pm', '8:30pm',
+  '9:15pm', '10pm', '11:15pm'
+];
+
+const WARDS_FROM_ISLAND_MON_FRI = ['6:45am', '7:15am', '7:45am', '8:30am', '9am', '10am', '11am', '12pm',
+  '1pm', '2pm', '3pm', '3:30pm', '4pm', '4:30pm', '5pm', '5:45pm', '6:30pm', '7:15pm', '8pm', '8:45pm',
+  '9:30pm', '10:15pm', '11:30pm'
+];
 
 export default class FerrySchedule extends React.Component {
-
+  static navigationOptions = {
+    header: null,
+    gesturesEnabled: false
+  }
 
 
   render() {
+    console.log('moment', moment().format());
     return (
-      <TouchableWithoutFeedback onPress={this.aboutUsNav}>
         <View style={[styles.beachBodyContent, styles.beachCard]}>
           <Text style={styles.eColiBold}>
             About the project
           </Text>
         </View>
-      </TouchableWithoutFeedback>
     );
   }
 }
